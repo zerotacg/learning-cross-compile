e74d7fc238cc84dba8015b11261fc989d7173764
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-01-18 20:45:12 +0100

    add hunter

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <openssl/evp.h>
 #include <openssl/rsa.h>
+#include <curl/curl.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Generating a Key" << std::endl;
+    std::cout << "Curl Version" << LIBCURL_VERSION << std::endl;
+    std::cout << "Creating ssl context" << std::endl;
 
     EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    CURL *curl_easy_init();
+
+    if(ctx) {
+        std::cout << "Contex created" << std::endl;
+    }
 
     return 0;
 }
