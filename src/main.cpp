a763b3e1a1a481508700385c1240f7050b176f5a
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-01-19 18:59:12 +0100

    remove code unnecessary to reproduce an issue with hunter openssl

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include <openssl/evp.h>
 #include <openssl/rsa.h>
-#include <curl/curl.h>
 
-int main()
-{
-    std::cout << "Curl Version" << LIBCURL_VERSION << std::endl;
+int main() {
     std::cout << "Creating ssl context" << std::endl;
 
     EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
-    CURL *curl_easy_init();
 
-    if(ctx) {
+    if (ctx) {
         std::cout << "Contex created" << std::endl;
     }
 
