72329dc258b0e544e830a87e917e3185a263eb0b
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-11-12 21:17:15 +0100

    calculate some simple hash

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <openssl/evp.h>
 #include <openssl/rsa.h>
+#include <openssl/sha.h>
 
 int main() {
     std::cout << "Creating ssl context" << std::endl;
 
-    EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    EVP_PKEY_CTX* context = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
 
-    if (ctx) {
-        std::cout << "Contex created" << std::endl;
+    if (context) {
+        std::cout << "Context created" << std::endl;
+        unsigned char hash[32];
+        SHA256("my hash input", sizeof hash, hash);
+
+        std::cout << "hash is: " << std::hex;
+        for (const auto& e : hash) {
+            std::cout << e;
+        }
+        std::cout << std::endl;
+    } else {
+        std::cerr << "Context could not be created" << std::endl;
     }
 
     return 0;
